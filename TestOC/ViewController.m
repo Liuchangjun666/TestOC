5113e8efa08860fd94e8703932819da91c66a6a8
ShaBro <shabro@shabrodeimac.local>
2016-07-15 13:38:05 +0800

    显示分数

--- TestOC/ViewController.m
+++ TestOC/ViewController.m
@@ -38,6 +38,17 @@
     [super viewDidLoad];
     
     
+    NSString *string = @"This is a string";
+    NSString *string2 = @"string";
+    NSRange range = [string rangeOfString:string2];
+    unsigned long location = range.location;
+    unsigned long lenght = range.length;
+    NSString *astring = [[NSString alloc] initWithString:[NSString stringWithFormat:@"Locaion:%lu,Lenght:%lu",location,lenght]];
+    NSLog(@"astring:%@",astring);
+    
+//    NSString *temp = string substringWithRange:NSMakeRange(index1, 4)
+//    NSString *trm = [string substringFromIndex:3];
+    
 }
 
 - (void)didReceiveMemoryWarning {
